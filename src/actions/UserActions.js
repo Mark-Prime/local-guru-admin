32b912903eee02a0af32fd45206886562524adeb
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-12-03 14:16:35 -0800

    Add empty state for followers page, fix new accounts missing followers field

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,66 +1,76 @@
-import { auth, db , functions, storage } from '../firebase'
+import { auth, db, functions, storage } from "../firebase";
 
-export const LOGIN_USER = 'login_user'
-export const LOGOUT_USER = 'logout_user'
+export const LOGIN_USER = "login_user";
+export const LOGOUT_USER = "logout_user";
 
-export function loginUser(){
+export function loginUser() {
   return dispatch => {
     return auth.onAuthStateChanged(user => {
       if (user) {
-        return db.collection('producers').doc(user.uid).get()
-        .then(userDoc => {
-          if(userDoc.exists){
-            return dispatch({
-              type: LOGIN_USER,
-              payload: userDoc.data()
-            })
-          } else {
-            return db.collection('producers').doc(user.uid).set({
-              displayName: user.displayName,
-              email: user.email,
-              uid: user.uid,
-            })
-            .then(userDoc => {
+        return db
+          .collection("producers")
+          .doc(user.uid)
+          .get()
+          .then(userDoc => {
+            if (userDoc.exists) {
               return dispatch({
                 type: LOGIN_USER,
                 payload: userDoc.data()
-              })
-            })
-          }
-        })
+              });
+            } else {
+              return db
+                .collection("producers")
+                .doc(user.uid)
+                .set({
+                  displayName: user.displayName,
+                  email: user.email,
+                  uid: user.uid,
+                  followers: []
+                })
+                .then(userDoc => {
+                  return dispatch({
+                    type: LOGIN_USER,
+                    payload: userDoc.data()
+                  });
+                });
+            }
+          });
       } else {
         return dispatch({
           type: LOGOUT_USER
-        })
+        });
       }
-    })
-  }
+    });
+  };
 }
 
-export function getProducer(){
+export function getProducer() {
   return dispatch => {
     return auth.onAuthStateChanged(user => {
-      if(user){
-        return db.collection('producers').doc(user.uid).get()
-        .then(doc => {
-          return dispatch({
-            type: LOGIN_USER,
-            payload: doc.data()
-          })
-        })
+      if (user) {
+        return db
+          .collection("producers")
+          .doc(user.uid)
+          .get()
+          .then(doc => {
+            return dispatch({
+              type: LOGIN_USER,
+              payload: doc.data()
+            });
+          });
       } else {
         return dispatch({
-          type: LOGIN_USER,
-        })
+          type: LOGIN_USER
+        });
       }
-    })
-  }
+    });
+  };
 }
 
-export function logoutUser(){
+export function logoutUser() {
   return dispatch => {
-    console.log('logging out')
-    auth.signOut()
+    console.log("logging out");
+    auth.signOut();
     // .then(() => {
     //   return dispatch({
     //     type: LOGOUT_USER
@@ -69,67 +79,86 @@ export function logoutUser(){
     // .catch(err => {
     //   console.error(err)
     // })
-  }
+  };
 }
 
-export function addPayoutCard(user, token){
+export function addPayoutCard(user, token) {
   return dispatch => {
-    console.log(token)
-    console.log(user)
-    return functions.httpsCallable('addPayoutCard')({ user: user, token: token.id })
-    .then(res => {
-      console.log(res)
-    })
-  }
+    console.log(token);
+    console.log(user);
+    return functions
+      .httpsCallable("addPayoutCard")({ user: user, token: token.id })
+      .then(res => {
+        console.log(res);
+      });
+  };
 }
 
-export function addBankAccount(user, token){
+export function addBankAccount(user, token) {
   return dispatch => {
-    console.log(token)
-    return functions.httpsCallable('addBankAccount')({ user: user, token: token.id })
-    .then(res => {
-      console.log(res)
-    })
-  }
+    console.log(token);
+    return functions
+      .httpsCallable("addBankAccount")({ user: user, token: token.id })
+      .then(res => {
+        console.log(res);
+      });
+  };
 }
 
-export function editProfile(user, bio, photo){
+export function editProfile(user, bio, photo) {
   return dispatch => {
-    console.log(photo)
-    return storage.ref().child(`producers/${photo.name}`).put(photo)
-    .then(snapshot => {
-      return snapshot.ref.getDownloadURL()
-    })
-    .then(photoURL => {
-      return db.collection('producers').doc(user).set({
-        photoURL: photoURL,
-        bio: bio
-      }, { merge: true })
-    })
-    .catch(err => console.log(err))
-  }
+    console.log(photo);
+    return storage
+      .ref()
+      .child(`producers/${photo.name}`)
+      .put(photo)
+      .then(snapshot => {
+        return snapshot.ref.getDownloadURL();
+      })
+      .then(photoURL => {
+        return db
+          .collection("producers")
+          .doc(user)
+          .set(
+            {
+              photoURL: photoURL,
+              bio: bio
+            },
+            { merge: true }
+          );
+      })
+      .catch(err => console.log(err));
+  };
 }
 
-export function createAccount(name, email, password, address, token){
-  console.log('name', name)
-  console.log('email', email)
+export function createAccount(name, email, password, address, token) {
+  console.log("name", name);
+  console.log("email", email);
   return dispatch => {
-    return auth.createUserWithEmailAndPassword(email, password)
-    .then(res => {
-      const { uid } = res.user;
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        const { uid } = res.user;
 
-      return db.collection('producers').doc(uid).set({
-        displayName: name,
-        uid: uid,
-        address: address,
-        token: token.token,
-        email: email
-      }, { merge: true })
-    })
-    .catch(error => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(`ERROR (${errorCode}): ${errorMessage}`)
-    })
-  }
+        return db
+          .collection("producers")
+          .doc(uid)
+          .set(
+            {
+              displayName: name,
+              uid: uid,
+              address: address,
+              token: token.token,
+              followers: [],
+              email: email
+            },
+            { merge: true }
+          );
+      })
+      .catch(error => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(`ERROR (${errorCode}): ${errorMessage}`);
+      });
+  };
 }
--- src/hooks/useFollowers.js
+++ src/hooks/useFollowers.js
@@ -11,21 +11,26 @@ const useFollowers = customers => {
 
     let userList = [];
 
-    customers.map(async user => {
-      try {
-        const doc = await db
-          .collection("users")
-          .doc(user)
-          .get();
-        if (subscribed) {
-          userList = [...userList, doc.data()];
-          setFollowers(userList);
-          setLoading(false);
+    if (customers.length > 0) {
+      customers.map(async user => {
+        try {
+          const doc = await db
+            .collection("users")
+            .doc(user)
+            .get();
+          if (subscribed) {
+            userList = [...userList, doc.data()];
+            setFollowers(userList);
+            setLoading(false);
+          }
+        } catch (err) {
+          setError(err);
         }
-      } catch (err) {
-        setError(err);
-      }
-    });
+      });
+    } else {
+      setLoading(false);
+      setFollowers([]);
+    }
 
     return () => (subscribed = false);
   }, [customers]);
