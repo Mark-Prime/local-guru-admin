cfd29869e887c629443e4a39d284ae09d034526b
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-12-20 15:22:57 -0800

    Add date to dispute card

--- src/components/Dispute.js
+++ src/components/Dispute.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import {
   Card,
   Avatar,
   TextStyle,
+  TextField,
   TextContainer,
   Heading,
   Badge,
   Stack
 } from "@shopify/polaris";
+import { TOGGLE_TOAST } from "../actions/UIActions";
 import Moment from "react-moment";
 
 const { Section } = Card;
@@ -23,37 +26,80 @@ const renderBadge = status => {
   }
 };
 
-const Dispute = ({ user, order, status }) => (
-  <Card
-    title={
-      <Stack>
-        <Heading>Dispute #104F84FBB03</Heading>
-        {status && renderBadge(status)}
-      </Stack>
-    }
-    actions={[]}
-    sectioned
-  >
-    <Section>
-      <Stack alignment="center">
-        <Avatar size="large" source={user.photoURL} name={user.name} />
-        <TextContainer spacing="tight">
-          <p>
-            <TextStyle variation="strong">{user.name}</TextStyle>
-          </p>
-          <p>
-            <TextStyle variation="subdued">#{order.id}</TextStyle>
-          </p>
-          <p>${order.total.toFixed(2)}</p>
+const Dispute = ({ user, order, status }) => {
+  const [response, setResponse] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    setResponse("");
+    dispatch({ type: TOGGLE_TOAST, payload: `Response submitted` });
+  };
+
+  return (
+    <Card
+      primaryFooterAction={
+        status === "awaiting" && {
+          content: "Submit response",
+          disabled: response === "",
+          onAction: () => handleSubmit()
+        }
+      }
+      title={
+        <Stack>
+          <TextContainer spacing="loose">
+            <Heading>Dispute #104F84FBB03</Heading>
+            <TextStyle variation="subdued">
+              <Moment format="LLL">{order.date}</Moment>
+            </TextStyle>
+          </TextContainer>
+
+          {status && renderBadge(status)}
+        </Stack>
+      }
+      actions={[]}
+      sectioned
+    >
+      <Section>
+        <Stack alignment="center">
+          <Avatar size="large" source={user.photoURL} name={user.name} />
+          <TextContainer spacing="tight">
+            <p>
+              <TextStyle variation="strong">{user.name}</TextStyle>
+            </p>
+            <p>
+              <TextStyle variation="subdued">#{order.id}</TextStyle>
+            </p>
+            <p>${order.total.toFixed(2)}</p>
+          </TextContainer>
+        </Stack>
+      </Section>
+      <Section title="Message">
+        <TextContainer>
+          <TextStyle variation="subdued">Lorem ipsum dolor sit amet</TextStyle>
         </TextContainer>
-      </Stack>
-    </Section>
-    <Section title="Message">
-      <TextContainer>
-        <TextStyle variation="subdued">Lorem ipsum dolor sit amet</TextStyle>
-      </TextContainer>
-    </Section>
-  </Card>
-);
+      </Section>
+      {status === "awaiting" && (
+        <Section title="Response">
+          <TextField
+            style={{ maxWidth: 400 }}
+            onChange={value => setResponse(value)}
+            multiline={3}
+            value={response}
+            maxLength="300"
+          />
+          <br />
+          <p>
+            <TextStyle
+              variation={response.length < 250 ? `subdued` : `negative`}
+            >
+              {300 - response.length} characters remaining
+            </TextStyle>
+          </p>
+        </Section>
+      )}
+    </Card>
+  );
+};
 
 export default Dispute;
--- src/pages/Orders/Disputes.js
+++ src/pages/Orders/Disputes.js
@@ -8,104 +8,119 @@ import {
   Badge,
   Link
 } from "@shopify/polaris";
+import styled from "styled-components";
 import { useLocation, useHistory } from "react-router-dom";
 import Dispute from "../../components/Dispute";
+
+const Wrapper = styled.div`
+  .Polaris-Card {
+    max-width: 800px;
+  }
+`;
 
 const Disputes = () => {
   const { pathname } = useLocation();
   const { push } = useHistory();
-  console.log("pathname", pathname);
-  console.log(pathname === "");
 
   return (
     <Page title="Disputes">
-      <TextContainer>
-        <Stack>
-          <Link onClick={() => push("/disputes/under-review")}>
-            <Heading>
-              <TextStyle variation="subdued">
-                Under Review <Badge status="info">1</Badge>
-              </TextStyle>
-            </Heading>
-          </Link>
-          <Link onClick={() => push("/disputes/awaiting-response")}>
-            <Heading>
-              <TextStyle variation="subdued">
-                Awaiting Response <Badge status="attention">1</Badge>
-              </TextStyle>
-            </Heading>
-          </Link>
-          <Link onClick={() => push("/disputes/resolved")}>
-            <Heading>
-              <TextStyle variation="subdued">
-                Resolved <Badge status="success">1</Badge>
-              </TextStyle>
-            </Heading>
-          </Link>
-        </Stack>
-      </TextContainer>
-      <br />
-      {(pathname === "/disputes" ||
-        pathname === "/disputes/awaiting-response") && (
-        <Dispute
-          status="awaiting"
-          user={{
-            name: `Test User`,
-            photoURL: `https://yt3.ggpht.com/a-/AAuE7mBPGLg7nB_q2ZKwk3Tp7xF35lHojY_w0Dogm8DT6A=s88-c-k-c0xffffffff-no-rj-mo`
-          }}
-          order={{
-            total: 23,
-            id: "AF57676JH",
-            date: Date.now(),
-            items: [
-              {
-                title: `Bananas`,
-                photo: `https://firebasestorage.googleapis.com/v0/b/local-guru-aeac9.appspot.com/o/products%2Fbananas.jpg?alt=media&token=d12239e3-4377-4d2d-a79f-1d47e453c9f0`
-              }
-            ]
-          }}
-        />
-      )}
-      {(pathname === "/disputes" || pathname === "/disputes/under-review") && (
-        <Dispute
-          status="review"
-          user={{
-            name: `Test User`,
-            photoURL: `https://yt3.ggpht.com/a-/AAuE7mBPGLg7nB_q2ZKwk3Tp7xF35lHojY_w0Dogm8DT6A=s88-c-k-c0xffffffff-no-rj-mo`
-          }}
-          order={{
-            total: 80,
-            id: "AF57676JH",
-            date: Date.now(),
-            items: [
-              {
-                title: `Bananas`,
-                photo: `https://firebasestorage.googleapis.com/v0/b/local-guru-aeac9.appspot.com/o/products%2Fbananas.jpg?alt=media&token=d12239e3-4377-4d2d-a79f-1d47e453c9f0`
-              }
-            ]
-          }}
-        />
-      )}
-      {(pathname === "/disputes" || pathname === "/disputes/resolved") && (
-        <Dispute
-          status="resolved"
-          user={{
-            name: `Test User`,
-            photoURL: `https://yt3.ggpht.com/a-/AAuE7mBPGLg7nB_q2ZKwk3Tp7xF35lHojY_w0Dogm8DT6A=s88-c-k-c0xffffffff-no-rj-mo`
-          }}
-          order={{
-            total: 23,
-            id: "AF57676JH",
-            date: Date.now(),
-            items: [
-              {
-                title: `Bananas`,
-                photo: `https://firebasestorage.googleapis.com/v0/b/local-guru-aeac9.appspot.com/o/products%2Fbananas.jpg?alt=media&token=d12239e3-4377-4d2d-a79f-1d47e453c9f0`
-              }
-            ]
-          }}
-        />
-      )}
+      <Wrapper>
+        <TextContainer>
+          <Stack>
+            <Link onClick={() => push("/disputes")}>
+              <Heading>
+                <TextStyle variation="subdued">
+                  All <Badge>3</Badge>
+                </TextStyle>
+              </Heading>
+            </Link>
+            <Link onClick={() => push("/disputes/under-review")}>
+              <Heading>
+                <TextStyle variation="subdued">
+                  Under Review <Badge status="info">1</Badge>
+                </TextStyle>
+              </Heading>
+            </Link>
+            <Link onClick={() => push("/disputes/awaiting-response")}>
+              <Heading>
+                <TextStyle variation="subdued">
+                  Awaiting Response <Badge status="attention">1</Badge>
+                </TextStyle>
+              </Heading>
+            </Link>
+            <Link onClick={() => push("/disputes/resolved")}>
+              <Heading>
+                <TextStyle variation="subdued">
+                  Resolved <Badge status="success">1</Badge>
+                </TextStyle>
+              </Heading>
+            </Link>
+          </Stack>
+        </TextContainer>
+        <br />
+        {(pathname === "/disputes" ||
+          pathname === "/disputes/awaiting-response") && (
+          <Dispute
+            status="awaiting"
+            user={{
+              name: `Test User`,
+              photoURL: `https://yt3.ggpht.com/a-/AAuE7mBPGLg7nB_q2ZKwk3Tp7xF35lHojY_w0Dogm8DT6A=s88-c-k-c0xffffffff-no-rj-mo`
+            }}
+            order={{
+              total: 23,
+              id: "AF57676JH",
+              date: Date.now(),
+              items: [
+                {
+                  title: `Bananas`,
+                  photo: `https://firebasestorage.googleapis.com/v0/b/local-guru-aeac9.appspot.com/o/products%2Fbananas.jpg?alt=media&token=d12239e3-4377-4d2d-a79f-1d47e453c9f0`
+                }
+              ]
+            }}
+          />
+        )}
+        {(pathname === "/disputes" ||
+          pathname === "/disputes/under-review") && (
+          <Dispute
+            status="review"
+            user={{
+              name: `Test User`,
+              photoURL: `https://yt3.ggpht.com/a-/AAuE7mBPGLg7nB_q2ZKwk3Tp7xF35lHojY_w0Dogm8DT6A=s88-c-k-c0xffffffff-no-rj-mo`
+            }}
+            order={{
+              total: 80,
+              id: "AF57676JH",
+              date: new Date(new Date().setDate(new Date().getDate() - 3)),
+              items: [
+                {
+                  title: `Bananas`,
+                  photo: `https://firebasestorage.googleapis.com/v0/b/local-guru-aeac9.appspot.com/o/products%2Fbananas.jpg?alt=media&token=d12239e3-4377-4d2d-a79f-1d47e453c9f0`
+                }
+              ]
+            }}
+          />
+        )}
+        {(pathname === "/disputes" || pathname === "/disputes/resolved") && (
+          <Dispute
+            status="resolved"
+            user={{
+              name: `Test User`,
+              photoURL: `https://yt3.ggpht.com/a-/AAuE7mBPGLg7nB_q2ZKwk3Tp7xF35lHojY_w0Dogm8DT6A=s88-c-k-c0xffffffff-no-rj-mo`
+            }}
+            order={{
+              total: 23,
+              id: "AF57676JH",
+              date: new Date(new Date().setDate(new Date().getDate() - 4)),
+              items: [
+                {
+                  title: `Bananas`,
+                  photo: `https://firebasestorage.googleapis.com/v0/b/local-guru-aeac9.appspot.com/o/products%2Fbananas.jpg?alt=media&token=d12239e3-4377-4d2d-a79f-1d47e453c9f0`
+                }
+              ]
+            }}
+          />
+        )}
+      </Wrapper>
     </Page>
   );
 };
