3a3e6d8a859bab208a4e7208004df51994e99e76
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-02-12 16:04:00 -0800

    Switch products page to hooks

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,9 +1,6 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import {
-  fetchUserProducts,
-  fetchAllProducts
-} from "../../actions/ProductActions";
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import {
   Page,
   Card,
@@ -12,12 +9,12 @@ import {
   Thumbnail,
   Pagination,
   EmptyState,
-  Link
+  Link,
+  Modal
 } from "@shopify/polaris";
+import { db } from "../../firebase";
 import emptyProducts from "../../assets/empty-products.svg";
 import styled from "styled-components";
-import { withRouter } from "react-router-dom";
-import PropTypes from "prop-types";
 
 const PaginationFooter = styled.div`
   display: flex;
@@ -26,92 +23,93 @@ const PaginationFooter = styled.div`
   height: 100px;
 `;
 
-class Products extends Component {
-  state = {
-    page: 1,
-    isLoaded: false,
-    modalOpen: false,
-    modalType: "",
-    selectedItems: [],
-    searchValue: "",
-    appliedFilters: []
-  };
+const Products = () => {
+  const [page, setPage] = useState(1);
+  const [loaded, setLoaded] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
 
-  componentDidMount() {
-    if (this.props.user.admin) {
-      this.props.fetchAllProducts().then(() => {
-        this.setState({ isLoaded: true });
-      });
-    } else {
-      this.props.fetchUserProducts(this.props.user.uid).then(() => {
-        this.setState({ isLoaded: true });
-      });
-    }
-  }
+  const user = useSelector(state => state.user);
+  const history = useHistory();
 
-  handleSearchChange = searchValue => {
-    this.setState({ searchValue });
-  };
+  useEffect(() => {
+    const fetchUserProducts = async () => {
+      let array = [];
 
-  handleFiltersChange = appliedFilters => {
-    this.setState({ appliedFilters });
-    if (appliedFilters.length > 0) {
-      this.props.fetchProducts({ limit: 50 }, appliedFilters);
-    } else {
-      this.props.fetchProducts({ limit: 50 });
-    }
-  };
+      const snapshot = await db
+        .collectionGroup("producers")
+        .where(`uid`, "==", user.uid)
+        .get();
 
-  handleSelectionChange = selectedItems => {
-    this.setState({ selectedItems });
-  };
+      if (!snapshot.empty) {
+        snapshot.forEach(doc => {
+          if (doc.data().product) {
+            array = [{ id: doc.data().uid, ...doc.data() }, ...array];
+          }
+        });
 
-  onPrev = () => {
+        setProducts(array);
+        setLoaded(true);
+      } else {
+        setLoaded(true);
+      }
+    };
+
+    fetchUserProducts();
+  }, [user]);
+
+  // const handleSearchChange = useCallback(searchValue => {
+  //   setSearchValue(searchValue);
+  // }, []);
+
+  const handleSelectionChange = useCallback(selection => {
+    console.log(selection);
+    setSelectedItems(selection);
+  }, []);
+
+  const onPrev = useCallback(() => {
     const { page } = this.state;
     const limit = 50;
-    const firstProduct = Object.keys(this.props.products)[0];
-    const offset = this.props.products[firstProduct].created_at;
+    const firstProduct = products[0];
+    const offset = products[firstProduct].created_at;
     console.log(`end before ${firstProduct}`);
     this.props.fetchProducts({ endBefore: offset, limit: limit }).then(() => {
       this.setState({ page: page - 1 });
       if (page === 2) {
-        this.props.history.push(`/products`);
+        history.push(`/products`);
       } else {
-        this.props.history.push(`/products/page/${this.state.page}`);
+        history.push(`/products/page/${this.state.page}`);
       }
     });
-  };
+  }, [history, products]);
 
-  onNext = () => {
-    const { page } = this.state;
+  const onNext = useCallback(() => {
     const limit = 50;
-    const lastProduct = Object.keys(this.props.products)[49];
+    const lastProduct = Object.keys(products)[49];
     const offset = this.props.products[lastProduct].created_at;
     console.log(`start after ${lastProduct}`);
     this.props.fetchProducts({ startAfter: offset, limit: limit }).then(() => {
       this.setState({ page: page + 1 });
-      this.props.history.push(`/products/page/${this.state.page}`);
+      setPage(page => page + 1);
+      history.push(`/products/page/${this.state.page}`);
     });
-  };
+  }, [history, page, products]);
 
-  handleModalToggle = type => {
-    console.log("toggle modal");
-    this.setState({ modalOpen: !this.state.modalOpen, modalType: type });
-  };
+  const handleModalToggle = useCallback(type => {
+    setModal(modal => !modal);
+  }, []);
 
-  renderItem = item => {
-    const { product, title, created_at, image } = item;
+  const renderItem = useCallback(item => {
+    console.log(item);
+    const { product, title, uid, image } = item;
     const media = <Thumbnail alt={title} source={image} />;
 
     return (
       <ResourceList.Item
-        id={created_at}
+        id={uid}
         media={media}
-        url={
-          this.props.user.admin
-            ? `/product/edit/${item.id}`
-            : `/product/edit/${product}`
-        }
+        url={`/product/edit/${product}`}
         accessibilityLabel={`View details for ${title}`}
       >
         <h3>
@@ -119,119 +117,97 @@ class Products extends Component {
         </h3>
       </ResourceList.Item>
     );
+  }, []);
+
+  const resourceName = {
+    singular: "product",
+    plural: "products"
   };
 
-  render() {
-    const resourceName = {
-      singular: "product",
-      plural: "products"
-    };
-
-    const promotedBulkActions = [
-      {
-        content: "Edit products",
-        onAction: () => this.handleModalToggle()
-      }
-    ];
-
-    // If the user has empty business details, disallow creating a product
-    let businessDetails = false;
-    if (this.props.user.businessDetails) {
-      const {
-        businessName,
-        fullName,
-        street,
-        city,
-        zip
-      } = this.props.user.businessDetails;
-
-      if (
-        businessName !== "" &&
-        fullName !== "" &&
-        street !== "" &&
-        zip !== "" &&
-        city !== ""
-      ) {
-        businessDetails = true;
-      }
+  const promotedBulkActions = [
+    {
+      content: "Delete"
     }
+  ];
 
-    return (
-      <Page
-        title="Products"
-        primaryAction={{
-          content: "Add Product",
-          url: "/products/add",
-          disabled: !businessDetails
-        }}
-      >
-        {this.state.isLoaded &&
-          (this.props.products.length > 0 ? (
-            <>
-              <>
-                {!businessDetails && (
-                  <Card sectioned title="Profile Incomplete">
-                    Your <Link url="/account">Business details</Link> must be
-                    complete in order to create products
-                  </Card>
-                )}
-              </>
-              <Card>
-                <ResourceList
-                  resourceName={resourceName}
-                  items={this.props.products}
-                  renderItem={this.renderItem}
-                  selectedItems={this.state.selectedItems}
-                  onSelectionChange={this.handleSelectionChange}
-                  promotedBulkActions={promotedBulkActions}
-                />
-                {this.props.products.length > 49 ? (
-                  <PaginationFooter>
-                    <Pagination
-                      hasPrevious={this.state.page > 1}
-                      previousKeys={[74]}
-                      previousTooltip="j"
-                      onPrevious={this.onPrev}
-                      hasNext
-                      nextKeys={[75]}
-                      nextTooltip="k"
-                      onNext={this.onNext}
-                    />
-                  </PaginationFooter>
-                ) : null}
-              </Card>
-            </>
-          ) : (
-            <EmptyState
-              action={{ content: "Add product", url: "/products/add" }}
-              secondaryAction={{
-                content: "Learn more",
-                url: "/help"
-              }}
-              heading="Manage your products"
-              image={emptyProducts}
-            >
-              <p>Add and edit your available products for customers.</p>
-            </EmptyState>
-          ))}
-      </Page>
-    );
+  // If the user has empty business details, disallow creating a product
+  let businessDetails = false;
+  if (user.businessDetails) {
+    const { businessName, fullName, street, city, zip } = user.businessDetails;
+
+    if (
+      businessName !== "" &&
+      fullName !== "" &&
+      street !== "" &&
+      zip !== "" &&
+      city !== ""
+    ) {
+      businessDetails = true;
+    }
   }
-}
 
-Products.propTypes = {
-  user: PropTypes.object.isRequired,
-  products: PropTypes.array.isRequired,
-  fetchUserProducts: PropTypes.func.isRequired,
-  history: PropTypes.object.isRequired
+  return (
+    <Page
+      title="Products"
+      primaryAction={{
+        content: "Add Product",
+        url: "/products/add",
+        disabled: !businessDetails
+      }}
+    >
+      {loaded &&
+        (products.length > 0 ? (
+          <>
+            <>
+              {!businessDetails && (
+                <Card sectioned title="Profile Incomplete">
+                  Your <Link url="/account">Business details</Link> must be
+                  complete in order to create products
+                </Card>
+              )}
+            </>
+            <Card>
+              <ResourceList
+                items={products}
+                resourceName={resourceName}
+                renderItem={renderItem}
+                selectedItems={selectedItems}
+                onSelectionChange={handleSelectionChange}
+                selectable
+                promotedBulkActions={promotedBulkActions}
+              />
+              {products.length > 49 ? (
+                <PaginationFooter>
+                  <Pagination
+                    hasPrevious={page > 1}
+                    previousKeys={[74]}
+                    previousTooltip="j"
+                    onPrevious={onPrev}
+                    hasNext
+                    nextKeys={[75]}
+                    nextTooltip="k"
+                    onNext={onNext}
+                  />
+                </PaginationFooter>
+              ) : null}
+            </Card>
+          </>
+        ) : (
+          <EmptyState
+            action={{ content: "Add product", url: "/products/add" }}
+            secondaryAction={{
+              content: "Learn more",
+              url: "/help"
+            }}
+            heading="Manage your products"
+            image={emptyProducts}
+          >
+            <p>Add and edit your available products for customers.</p>
+          </EmptyState>
+        ))}
+      <Modal open={modal}></Modal>
+    </Page>
+  );
 };
 
-export default withRouter(
-  connect(
-    (state, ownProps) => ({
-      products: state.products,
-      user: state.user
-    }),
-    { fetchUserProducts, fetchAllProducts }
-  )(Products)
-);
+export default Products;
